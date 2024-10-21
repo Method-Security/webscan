ec94bdb1b32bd59aaa869eaaec8a728052090fbd
Sam Jones <sam@method.security>
2024-10-21 11:21:05 -0500

    fern

--- generated/go/option/request_option.go
+++ generated/go/option/request_option.go
@@ -3,10 +3,9 @@
 package option
 
 import (
+	core "github.com/Method-Security/webscan/generated/go/core"
 	http "net/http"
 	url "net/url"
-
-	core "github.com/Method-Security/webscan/generated/go/core"
 )
 
 // RequestOption adapts the behavior of an indivdual request.
