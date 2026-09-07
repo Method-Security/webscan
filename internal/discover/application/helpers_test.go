61f0b8d89aacf672029ece9f2e2ed6835a369e3a
Philip Ayoub <philip@method.security>
2026-09-07 11:15:14 -0400

    Add CDN Application Fingeprints

--- internal/discover/application/helpers_test.go
+++ internal/discover/application/helpers_test.go
@@ -29,3 +29,27 @@ func TestApplicationResourceTypeLoadBalancer(t *testing.T) {
 		t.Fatalf("resourceType = %q, want %q", resourceType, discover.ApplicationResourceTypeLoadBalancer)
 	}
 }
+
+func TestGetTemplatePathsContentDeliveryNetwork(t *testing.T) {
+	resourceType := &discover.ApplicationResourceConfigType{
+		ApplicationResourceType: discover.ApplicationResourceTypeContentDeliveryNetwork,
+	}
+
+	paths, err := getTemplatePaths(resourceType)
+	if err != nil {
+		t.Fatalf("getTemplatePaths returned error: %v", err)
+	}
+	if len(paths) != 1 || paths[0] != "discover/application/contentdeliverynetwork" {
+		t.Fatalf("getTemplatePaths = %v, want [discover/application/contentdeliverynetwork]", paths)
+	}
+}
+
+func TestApplicationResourceTypeContentDeliveryNetwork(t *testing.T) {
+	resourceType, err := discover.NewApplicationResourceTypeFromString("CONTENT_DELIVERY_NETWORK")
+	if err != nil {
+		t.Fatalf("NewApplicationResourceTypeFromString returned error: %v", err)
+	}
+	if resourceType != discover.ApplicationResourceTypeContentDeliveryNetwork {
+		t.Fatalf("resourceType = %q, want %q", resourceType, discover.ApplicationResourceTypeContentDeliveryNetwork)
+	}
+}
